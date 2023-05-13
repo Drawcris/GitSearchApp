80a7bf907a0331bb2b6f45eec0b02559e954ec38
Drawcris <sirwoj2@wp.pl>
2023-05-13 15:01:40 +0200

    Chyba poprawny NVVM

---
+++ GitSearch/View/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Diagnostics;
+
+
+using GitSearch.ViewModel;
+
+
+namespace GitSearch
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        // Button Szukaj
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FunkcjaVM.Funckja(avatarImage, wynikListBox, accountNameTextBox, resultsTextBlock);
+           
+
+        }
+        //
+
+        // button szukaj enterem
+        private void Enter_Click(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                FunkcjaVM.Funckja(avatarImage, wynikListBox, accountNameTextBox, resultsTextBlock);
+
+            }
+
+        }
+        //
+
+        // button wyjdz
+        private void Button_Close(object sender, RoutedEventArgs e)
+        {
+
+            Application.Current.Shutdown();
+
+        }
+        //
+
+        // Otwórz URL
+        private void wynikListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (wynikListBox.SelectedItem != null)
+            {
+                ListBoxItem item = wynikListBox.SelectedItem as ListBoxItem;
+                string url = GetUrlFromListBoxItem(item);
+                Process.Start("cmd.exe", $"/C start {url}");
+            }
+        }
+
+        private string GetUrlFromListBoxItem(ListBoxItem item)
+        {
+            string content = item.Content.ToString();
+            int startIndex = content.IndexOf("Link do repozytorium: ") + "Link do repozytorium: ".Length;
+            int endIndex = content.IndexOf("\n", startIndex);
+            return content.Substring(startIndex, endIndex - startIndex);
+        }
+        //
+
+       
+
+
+    }
+}
--- GitSearch/ViewModel/Funkcja.cs
+++ GitSearch/ViewModel/Funkcja.cs
@@ -10,79 +10,15 @@ using System.IO;
 using System.Diagnostics;
 
 
-namespace GitSearch
+
+using GitSearch.Model;
+
+
+namespace GitSearch.ViewModel
 {
-    public partial class MainWindow : Window
+
+    public class FunkcjaVM
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-
-        }
-
-        // Button Szukaj
-        private async void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-            Funckja(avatarImage, wynikListBox, accountNameTextBox, resultsTextBlock);
-
-        }
-        //
-
-        // button szukaj enterem
-        private void Enter_Click(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                Funckja(avatarImage, wynikListBox, accountNameTextBox, resultsTextBlock);
-
-            }
-
-        }
-        //
-
-        // button wyjdz
-        private void Button_Close(object sender, RoutedEventArgs e)
-        {
-
-            Application.Current.Shutdown();
-
-        }
-        //
-
-        // Otwórz URL
-        private void wynikListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-            if (wynikListBox.SelectedItem != null)
-            {
-                ListBoxItem item = wynikListBox.SelectedItem as ListBoxItem;
-                string url = GetUrlFromListBoxItem(item);
-                Process.Start("cmd.exe", $"/C start {url}");
-            }
-        }
-
-        private string GetUrlFromListBoxItem(ListBoxItem item)
-        {
-            string content = item.Content.ToString();
-            int startIndex = content.IndexOf("Link do repozytorium: ") + "Link do repozytorium: ".Length;
-            int endIndex = content.IndexOf("\n", startIndex);
-            return content.Substring(startIndex, endIndex - startIndex);
-        }
-        //
-
-        public class Repository
-        {
-
-
-            public string Name { get; set; }
-            public string Description { get; set; }
-            public string html_url { get; set; }
-            public string avatar_url { get; set; }
-            public DateTime created_at { get; set; }
-            public string login { get; set; }
-            public int id { get; set; }
-        }
-
         public async static void Funckja(Image avatarImage, ListBox wynikListBox, TextBox accountNameTextBox, TextBlock resultsTextBlock)
         {
             try
@@ -154,10 +90,5 @@ namespace GitSearch
                 MessageBox.Show($"Zła nazwa konta!", "Hamuj się!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
-
-
-
-
-
     }
 }
